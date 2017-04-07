43ba42fa7511b19927327f70c7f85ce9008bb671
Sharinda <sharinda7@gmail.com>
2017-04-07 16:54:51 +0300

    Add files via upload

---
+++ iRent/TenantViewHousingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TenantViewHousingViewController.swift
+//  iRent
+//
+//  Created by Edwin Mukenya on 3/3/17.
+//  Copyright © 2017 Edwin Mukenya. All rights reserved.
+//
+
+import UIKit
+
+class TenantViewHousingViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.loadRequest(NSURLRequest(url: NSURL(string: "https://njati.000webhostapp.com/iRent/tenant_view_houses.php")! as URL) as URLRequest)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
